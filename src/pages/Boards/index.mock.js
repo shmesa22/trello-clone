21a942afbe24194ed2095505f4f2bb66a0090220
Simon Hoyos <shmesa23@gmail.com>
2018-11-03 12:01:43 -0500

    Clean code

--- src/pages/Boards/index.mock.js
+++ src/pages/Boards/index.mock.js
@@ -5,38 +5,5 @@ export const starredBoards = {
 
 export const personalBoards = {
   title: 'Personal Boards',
-  icon:'far far-user',
+  icon: 'far far-user',
 };
-
-export const cards = [
-  {
-    id: 1,
-    title: 'trello clone',
-    isFavorited: true,
-    background: 'orange',
-  },
-  {
-    id: 2,
-    title: 'React JS',
-    isFavorited: true,
-    background: 'pink',
-  },
-  {
-    id: 3,
-    title: 'Meetup',
-    isFavorited: true,
-    background: 'red',
-  },
-  {
-    id: 4,
-    title: 'Programming',
-    isFavorited: false,
-    background: 'purple',
-  },
-  {
-    id: 5,
-    title: 'Learning',
-    isFavorited: false,
-    background: 'green',
-  }
-];
